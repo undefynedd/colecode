44b22b7974864c937b95ca6ce5adea84b5f7e436
cole <chloe.peiying.lin@gmail.com>
2024-06-26 23:34:52 -0700

    added day of week detection

--- uwu.js
+++ uwu.js
@@ -1,7 +1,7 @@
-let a = new Date();
+let today = new Date("March 13, 2024 00:00:00"); // whatevers after the 1 is the day
 
 class time {
-    constructor(_hour, _minute = 0, _second = 0) {
+    constructor(_hour, _minute, _second = 0) {
         this.hour = _hour;
         this.minute = _minute;
         this.second = _second;
@@ -20,31 +20,74 @@ class time {
     }
 }
 
-let schedule = [
+let schedule_normal = [
     {
         name: "Block A",
-        time: new time(8),
+        start: new time(8, 30),
+        end: new time(9, 50),
     },
 
     {
         name: "Block B",
-        time: new time(12, 1, 1),
+        start: new time(10, 00),
+        end: new time(11, 20),
     },
 
     {
         name: "Block C",
-        time: new time(12, 34, 3),
+        start: new time(11, 30),
+        end: new time(12, 50),
     },
 
     {
         name: "Block D",
-        time: new time(16, 27, 50),
-    },
-    {
-        name: "Block E",
-        time: new time(16, 27, 52),
+        start: new time(13, 40),
+        end: new time(15, 00),
     },
 ];
+
+let schedule_plt = [
+    {
+        name: "PLT",
+        start: new time(8, 30),
+        end: new time(9, 30),
+    },
+
+    {
+        name: "Block A",
+        start: new time(9, 35),
+        end: new time(10, 40),
+    },
+
+    {
+        name: "Block B",
+        start: new time(10, 50),
+        end: new time(11, 55),
+    },
+
+    {
+        name: "Block C",
+        start: new time(12, 40),
+        end: new time(13, 45),
+    },
+
+    {
+        name: "Block D",
+        start: new time(13, 55),
+        end: new time(15, 00),
+    },
+];
+
+let schedule = [];
+
+if ([1, 4, 5].includes(today.getDay())) {
+    schedule = schedule_normal;
+} else if ([2, 3].includes(today.getDay())) {
+    schedule = schedule_plt;
+} else {
+    // weekend
+    schedule = [];
+}
 
 function asdf() {
     var audio = new Audio('bell.mp3');
@@ -68,20 +111,28 @@ for (let leg of schedule) {
 for (let i = 0; i < schedule.length; i++) {
     let eventElement = document.createElement("div");
 
-    eventElement.innerText = schedule[i].time.getFormattedTime();
+    eventElement.innerText = schedule[i].end.getFormattedTime();
 
     document.getElementsByTagName("body")[0].appendChild(eventElement);
 }
 
+function getMsSinceMidnight(d) {
+  var e = new Date();
+  return d - e.setHours(0,0,0,0);
+}
+
 setInterval(function () {
-    let now = new Date();
+    // now is "today" + the actual time
+    let now = new Date(today.getTime() + getMsSinceMidnight(new Date()));
+
+    // the time 5 minutes from now should be equal to the end time of the block
     let more = new Date(now - 60000 * -5);
-    console.debug(more.toTimeString());
+    console.debug(`the time 5 minutes from now is ${more.toTimeString()}`);
     for (let i = 0; i < schedule.length; i++) {
         if (
-            more.getHours() == schedule[i].time.hour &&
-            more.getMinutes() == schedule[i].time.minute &&
-            more.getSeconds() == schedule[i].time.second
+            more.getHours() == schedule[i].end.hour &&
+            more.getMinutes() == schedule[i].end.minute &&
+            more.getSeconds() == schedule[i].end.second
         ) {
             var bell = new Audio('bell.mp3');
             bell.play();
